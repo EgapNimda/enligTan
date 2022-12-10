e10ed4a429f615b42c5b2cd009ef3bafe03d395b
EgapNimda <6434449023@student.chula.ac.th>
2022-12-10 16:37:20 +0700

    t

--- Enlighttan/authentication/models.py
+++ Enlighttan/authentication/models.py
@@ -1,2 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+class myuser():
+    stat = models.CharField(max_length=30)
--- Enlighttan/menu/models.py
+++ Enlighttan/menu/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class chant(models.Model):
-    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
     chanting = models.CharField(max_length=1000)
 

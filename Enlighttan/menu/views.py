cbc52d4f9aa25f42e4ef7cf5d7d6decbf863b3f1
EgapNimda <6434449023@student.chula.ac.th>
2022-12-10 16:02:33 +0700

    stuffs

--- Enlighttan/menu/views.py
+++ Enlighttan/menu/views.py
@@ -1,15 +1,7 @@
 from django.shortcuts import render
-'''from menu.time import monk_day'''
 # Create your views here.
 
 def home(request):
     return render(request, "main.html")
-    '''s = ""
-    if monk_day() :
-        s = "วันนี้วันพระ"
-    context = {
-        "monk" : s
-    }
-    return render(request, 'main.html')'''
 
 

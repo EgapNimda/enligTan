f8576bc70b9bc11c56461a2fe9551c91c239091b
EgapNimda <6434449023@student.chula.ac.th>
2022-12-10 13:07:37 +0700

    categorize signin and signup into account category

--- Enlighttan/authentication/urls.py
+++ Enlighttan/authentication/urls.py
@@ -4,5 +4,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('signin', views.signin),
+    path('account/', views.account),
+    path('account/signin', views.signin),
+    path('account/signup', views.signup),
 ]
--- Enlighttan/authentication/views.py
+++ Enlighttan/authentication/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+def account (request):
+    return render(request, "signIn.html")
+
+
 def signin (request):
     return render(request,"signIn.html")
+
+
+def signup (request):
+    return render(request, "signUp.html")

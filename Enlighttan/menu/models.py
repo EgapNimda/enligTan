d6bfe34b045398131d5da4b57fe6df1761e49b3b
punnuund <banthita.pun@gmail.com>
2022-12-12 02:48:35 +0700

    kuyy

--- Enlighttan/menu/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class chant(models.Model):
-    title = models.CharField(max_length=50)
-    chanting = models.CharField(max_length=1000)
-
